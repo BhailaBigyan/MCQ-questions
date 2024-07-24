6db000bffe2d5a8fd8a14d50c09ae794d12694e3
Bigyan Bhaila <93475413+BhailaBigyan@users.noreply.github.com>
2024-07-24 22:05:28 +0545

    Add files via upload

---
+++ leaderboard.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 24, 2024 at 06:20 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `leaderboard`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leader_board`
+--
+
+CREATE TABLE `leader_board` (
+  `name` varchar(50) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `indx` int(11) NOT NULL,
+  `que` varchar(224) NOT NULL,
+  `ans` varchar(20) NOT NULL,
+  `ans1` varchar(11) NOT NULL,
+  `ans2` varchar(11) NOT NULL,
+  `correct` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`indx`, `que`, `ans`, `ans1`, `ans2`, `correct`) VALUES
+(1, 'what is 1+1?', '0', '1', '2', 'c'),
+(2, 'what is 2+2?', '1', '2', '4', 'c');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
